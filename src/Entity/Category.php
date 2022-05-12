145b5641c2e7e44df3041df6a75573dc6a5cd92a
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2022-05-12 22:10:59 +0200

    [WL-33] - add category

---
+++ src/Entity/Category.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Ferdyrurka\RssReader\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ferdyrurka\RssReader\Repository\CategoryRepository;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
